bddd5cc59bd12693bade6a1e4d13de7c5a1c388e
Anton Burchak <anton.burchak@gmail.com>
2020-02-18 19:29:57 +0200

    fixed maximum call steack resize

--- changeListener.js
+++ changeListener.js
@@ -1,54 +1,75 @@
-
-
-Array.prototype.push = function (el) {
-
-    if (this.setListener === 'push') {
-        this[this.length] = this.callback(el) || el;
+// jshint esversion:6
+let setTypes = [];
+Array.prototype.push = function(el) {
+    // console.log(setTypes);
+    if (setTypes.length) {
+        setTypes.forEach(element => {
+            if (element[0] === 'push') {
+                this[this.length] = element[1](el) || el;
+            } else {
+                this[this.length] = el;
+            }
+        });
     } else {
         this[this.length] = el;
     }
+
     return this;
 };
 
-Array.prototype.setListener = function (changeType) {
-    this.addChangeListener(changeType);
+Array.prototype.pop = function(el) {
+
+    el = this[this.length - 1];
+
+
+    let tmp = [];
+
+    for (let i = 0; i < this.length - 1; i++) {
+        tmp[tmp.length] = this[i];
+    }
+    this.length = 0;
+    for (let i = 0; i < tmp.length; i++) {
+        this[i] = tmp[i];
+    }
+
+    if (setTypes.length) {
+        setTypes.forEach(element => {
+            if (element[0] === 'pop') {
+                element[1](el);
+            }
+        });
+    }
+
+    return this;
 };
+
+// Array.prototype.setListeners = function (changeType) {
+//     this.addChangeListener(changeType);
+
+// };
 
 Array.prototype.print = function() {
-    for (let i = 0; i < this.length; i++) {
-        console.log(this[i]);
-        
-    }
+    console.log(this);
 };
 
-Array.prototype.addChangeListener = function (changeType, callback) {
-    this.setListener = changeType;
+Array.prototype.addChangeListener = function(changeType, callback) {
+    setTypes[setTypes.length] = [changeType, callback];
+
+    this.setListeners = changeType;
     this.callback = callback;
 };
 
-let arr = [1, 2, 3];
-arr.addChangeListener('push', function (el) {console.log(`Вы изменили ${this} добавив элемент ${el}`)});
-arr.push(44);
+let arr = [],
+    arr2 = [];
+arr2.addChangeListener('pop', (el) => {
+    arr.push(el);
+    return console.log(arr);
+});
 
-
-arr.push(12);
-arr.push(5);
-
-
-arr.push(5);
-arr.push(5);
-
-
-
-
-// arr.print(1);
-
-
-
-arr.print();
-
-// arr.print();
-
-// function addChangeListener() {
-//     return console.log(this, arguments[0]);
-// } 
+arr2.push(1);
+arr2.push(2);
+arr2.push(3);
+console.log(arr2);
+arr2.pop();
+arr2.pop();
+arr2.pop();
